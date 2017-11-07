ad7f17d7d1a239ba292da49f7f0f42db00c95d27
Abraham Castro <cucharadepalo@gmail.com>
2017-11-07 01:10:24 +0100

    Working but not properly

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,13 +3,13 @@ class Elevator {
     this.floor = 0;
     this.MAXFLOOR = 10;
     this.requests = [];
-    this.direction = 'idle';
+    this.direction = 'up';
     this.waitingList = [];
     this.passengers = [];
   }
 
   start() {
-    let elevatorInterval = setInterval(() => {
+    this.elevatorInterval = setInterval(() => {
       this.update();
     }, 1 * 1000);
   }
@@ -18,15 +18,33 @@ class Elevator {
   }
   update() {
     this.log();
+    if (this.requests.length > 0) {
+      this.stop();
+      if (this.requests[0] == this.floor) {
+        this._passengersLeave();
+        this._passengersEnter();
+        this.direction == 'up' ? this.floorUp(): this.floorDown();
+        this.requests.shift();
+      } else if (this.requests[0] > this.floor) {
+        this.floorUp();
+        this.direction = 'up';
+      } else {
+        this.floorDown();
+        this.direction = 'down';
+      }
+      this.elevatorInterval = setTimeout(() => {
+        this.start();
+      }, 1 * 1000);
+    }
   }
   _passengersEnter() {
     if (this.requests.indexOf(this.floor) != -1) {
         for (let p = 0; p < this.waitingList.length; p++) {
           if (this.waitingList[p].originFloor == this.floor) {
             this.passengers.push(this.waitingList[p]);
-            this.waitingList.splice(p, 1);
-            this.requests.push(this.waitingList[p].destinationFloor);
+            this.requests.unshift(this.waitingList[p].destinationFloor);
             console.log(`${this.waitingList[p].name} has enter the elevator`);
+            this.waitingList.splice(p, 1);
           }
         }
     }
@@ -35,29 +53,24 @@ class Elevator {
     if (this.requests.indexOf(this.floor) != -1) {
       for (let p = 0; p < this.passengers.length; p++) {
         if (this.passengers[p].destinationFloor == this.floor) {
-          this.passengers.splice(p, 1);
           console.log(`${this.passengers[p].name} has left the elevator`);
+          this.passengers.splice(p, 1);
         }
       }
     }
   }
   floorUp() {
-    this.floor < 10 ? this.floor++ : this.floor;
+    if (this.floor < this.MAXFLOOR) this.floor++;
   }
   floorDown() {
-    this.floor > 0 ? this.floor-- : this.floor;
+    if (this.floor > 0 ) this.floor--;
   }
   call(person) {
     this.waitingList.push(person);
     this.requests.push(person.originFloor);
   }
   log() {
-    if (this.requests.length > 0 && this.requests[0] > this.floor) {
-      this.direction = 'up';
-    } else if (this.requests.length > 0 && this.requests[0] < this.floor) {
-      this.direction = 'down';
-    }
-    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor} | ${this.requests}`);
   }
 }
 
--- starter_code/index.js
+++ starter_code/index.js
@@ -3,30 +3,16 @@ const Person = require('./person.js');
 
 // First: Elevator
 let elevator = new Elevator();
-// Then: People
-let person1 = new Person('Julia', 1, 7);
-let person2 = new Person('Paco', 0, 5);
-let person3 = new Person('Carmen', 6, 0);
-let person4 = new Person('Andrea', 3, 10);
-let person5 = new Person('Irati', 1, 4);
-let person6 = new Person('Curro', 4, 8);
-let person7 = new Person('Andrea', 8, 0);
-let person8 = new Person('Sigrid', 6, 1);
-let person9 = new Person('Álvaro', 0, 9);
-let person10 = new Person('Alfonso', 1, 5);
 // Start the elevator
 elevator.start();
-elevator.call(person1);
-elevator.call(person2);
-elevator.call(person3);
-elevator.call(person4);
-elevator.call(person5);
-elevator.call(person6);
-elevator.call(person7);
-elevator.call(person8);
-elevator.call(person9);
-elevator.call(person10);
-console.log(elevator.waitingList);
-console.log(elevator.requests);
-
-elevator.stop();
+// Create people calls
+setTimeout(() => {elevator.call(new Person('Julia', 1, 7));}, 2000);
+setTimeout(() => {elevator.call(new Person('Paco', 0, 5));}, 2500);
+setTimeout(() => {elevator.call(new Person('Carmen', 6, 0));}, 3000);
+setTimeout(() => {elevator.call(new Person('Andrea', 3, 10));}, 3000);
+setTimeout(() => {elevator.call(new Person('Irati', 1, 4));}, 4000);
+setTimeout(() => {elevator.call(new Person('Curro', 4, 8));}, 6000);
+setTimeout(() => {elevator.call(new Person('Andrea', 8, 0));}, 8000);
+setTimeout(() => {elevator.call(new Person('Sigrid', 6, 1));}, 9000);
+setTimeout(() => {elevator.call(new Person('Álvaro', 0, 9));}, 9500);
+setTimeout(() => {elevator.call(new Person('Alfonso', 1, 5));}, 10000);
